9502748e9655513ede2fd48956734e4b7a126142
Murad <119056667+rmurad-ux@users.noreply.github.com>
2026-03-20 23:11:25 +0400

    Update task 1.ino

--- task1/task 1.ino
+++ task1/task 1.ino
@@ -1,1 +1,21 @@
+const int LED1 = 8;
+const int LED2 = 9;
+const int LED3 = 10;
 
+void setup(){
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(LED1, HIGH); digitalWrite(LED2, LOW); digitalWrite(LED3, LOW);
+  delay(300);
+
+  digitalWrite(LED1, LOW); digitalWrite(LED2, HIGH); digitalWrite(LED3, LOW);
+  delay(300);
+
+  digitalWrite(LED1, LOW); digitalWrite(LED2, LOW); digitalWrite(LED3, HIGH);
+  delay(300);
+  
+}
