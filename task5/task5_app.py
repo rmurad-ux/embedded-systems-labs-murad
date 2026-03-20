14a4393ff9edadd2abf4d5c77913270d8f50b6ce
Murad <119056667+rmurad-ux@users.noreply.github.com>
2026-03-20 23:10:20 +0400

    Update task5_app.py

--- task5/task5_app.py
+++ task5/task5_app.py
@@ -9,49 +9,54 @@ from PyQt6.QtCore import QTimer, Qt
 from PyQt6.QtGui import QFont
 
 ser = serial.Serial("COM12", 115200, timeout=1)
-time.sleep(2) #pause for 2 seconds, because arduino may reboot
+time.sleep(2) //pause for 2 seconds, because arduino may reboot
 
-class SerialGUI(QWidget):
-    def __init__(self): # self refers to our gui window
-        super().__init__()#creates the basic widget behavior
+class SerialGUI(QWidget): // creating the main GUI window
+    def __init__(self):  // self refers to our gui window
+        super().__init__() // creates the basic widget behavior
 
         self.setWindowTitle("Lab 5 Sound Monitor")
         self.resize(500, 420)
 
-        layout = QVBoxLayout()
+        layout = QVBoxLayout() // creating vertical layout
         layout.setSpacing(15)
         layout.setContentsMargins(20, 20, 20, 20)
 
-        title = QLabel("Sound Sensor Monitor")
+        title = QLabel("Sound Sensor Monitor") // title 
         title.setFont(QFont("Arial", 18, QFont.Weight.Bold))
         title.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(title) #display the title from the QLabel
+        layout.addWidget(title) // display the title from the QLabel
+        
+// Information Labels
 
-        self.level_label = QLabel("Level: -") # without self, level_label would be a temporary local variable
-        self.threshold_label = QLabel("Threshold: -") # using self to use it outside of init
+        self.level_label = QLabel("Level: -")  // without self, level_label would be a temporary local variable
+        self.threshold_label = QLabel("Threshold: -") // using self to use it outside of init
         self.events_label = QLabel("Events: -")
         self.db_label = QLabel("dB: -")
 
-        for lbl in [
+        for lbl in [ // styling the labels and applying parameters to all of them
             self.level_label,
             self.threshold_label,
             self.events_label,
             self.db_label
         ]:
             lbl.setFont(QFont("Arial", 13))
-            layout.addWidget(lbl) #addWidget => function, layout is a variable name created in QVBoxLayout
+            layout.addWidget(lbl) // addWidget function, layout is a variable name created in QVBoxLayout
 
+// Progress bar title
         self.bar_title = QLabel("Live Sound Level")
         self.bar_title.setFont(QFont("Arial", 13, QFont.Weight.Bold))
         layout.addWidget(self.bar_title)
-
+// Progress bar itself
         self.level_bar = QProgressBar()
         self.level_bar.setMinimum(0)
         self.level_bar.setMaximum(10)
         self.level_bar.setValue(0)
-        self.level_bar.setFormat("%v") #displays the numeric value
+        self.level_bar.setFormat("%v") // displays the numeric value
         self.level_bar.setMinimumHeight(28)
         layout.addWidget(self.level_bar)
+        
+	    // Loud and Quiet indicators inside the Progress bar
 
         self.status_box = QLabel("QUIET")
         self.status_box.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -63,45 +68,45 @@ class SerialGUI(QWidget):
         )
         layout.addWidget(self.status_box)
 
-        self.setLayout(layout) #attaches vertical layout to our gui
+        self.setLayout(layout) //attaches vertical layout to our gui
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.read_serial)
-        self.timer.start(100) #every 1ooms timer emit timeout and read_serial runs
+        self.timer.start(100) //every 1ooms timer emit timeout and read_serial runs
 
-    def read_serial(self):
-        if ser.in_waiting > 0: #ser is python's connection to arduino (com17, 115200....)
-            line = ser.readline().decode(errors="ignore").strip() #strip removes \n, decode bytes into a string
+def read_serial(self):
+        if ser.in_waiting > 0: // ser is python's connection to arduino (com17, 115200....) and it checks whether the serial data is available
+            line = ser.readline().decode(errors="ignore").strip() //strip removes \n, decode bytes into a string
 
             if line:
-                parts = line.split(",")
-
-                if len(parts) == 5: # 5 fields (level, db, treshold, above, events)
+                parts = line.split(",") // separates the received fields
+				
+                if len(parts) == 5: // 5 fields (level, db, treshold, above, events)
                     level = parts[0].strip()
                     db = parts[1].strip()
                     threshold = parts[2].strip()
                     above = parts[3].strip()
                     events = parts[4].strip()
 
-                    self.level_label.setText(level) #settext to change the text displayed by QLabel
+                    self.level_label.setText(level) //settext to change the text displayed by QLabel
                     self.threshold_label.setText(threshold)
                     self.events_label.setText(events)
                     self.db_label.setText(db)
 
                     try:
-                        level_value = int(level.split("=")[1]) #"Level", "8", 1=> take the second part
+                        level_value = int(level.split("=")[1]) //"Level", "8", 1=> take the second part
                         threshold_value = int(threshold.split("=")[1])
-
-                        if level_value < 0:
+// limits the bar value by keeping it betwen 0 and 10
+                        if level_value < 0: 
                             level_value = 0
                         if level_value > 10:
                             level_value = 10
 
-                        self.level_bar.setValue(level_value)
+                        self.level_bar.setValue(level_value)// updates progress bar
                         if level_value >= threshold_value:
                             self.level_bar.setStyleSheet("""
                                 QProgressBar {
-                                    border: 2px solid gray;
+                                    border: 2px solid gray; // color of progress bar
                                     border-radius: 5px;
                                     text-align: center;
                                 }
@@ -123,7 +128,7 @@ class SerialGUI(QWidget):
                     except:
                         pass
 
-                    if "Above=1" in above:
+                    if "Above=1" in above: // it is updating LOUD/QUIET status
                         self.status_box.setText("LOUD")
                         self.status_box.setStyleSheet(
                             "background-color: red; color: white; border: 2px solid black;"
